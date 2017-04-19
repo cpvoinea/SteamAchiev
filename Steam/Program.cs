a4e183fd454ded289ec7dff4924c0c182ac8478f
catalin <catalin@DESKTOP>
2017-04-20 00:27:44 +0300

    update

--- Steam/Program.cs
+++ Steam/Program.cs
@@ -23,91 +23,13 @@ namespace Steam
             }
         }
 
-        static int Percent(int count, int total)
-        {
-            return (int)Math.Round(count * 100.0 / total, 0);
-        }
-
-        static string ReadFile(string path)
-        {
-            if (!File.Exists(path))
-                return "";
-            using (StreamReader sr = new StreamReader(path))
-            {
-                string result = sr.ReadToEnd();
-                sr.Close();
-                return result;
-            }
-        }
-
         static void Main(string[] args)
         {
-            string steamId = SteamId;
-            string dataPath = "";
-            bool load = false;
-            int threshold = 0;
-            #region read params
-            // steam id
-            if (args == null || args.Length < 1)
-            {
-                Console.Write("Steam ID (leave empty to use {0}'s ID): ", KeyDomain);
-                string sid = Console.ReadLine();
-                if (!string.IsNullOrEmpty(sid))
-                    steamId = sid;
-            }
-            else if (!string.IsNullOrEmpty(args[0]))
-                steamId = args[0];
-
-            // load & load path
-            if (args == null || args.Length < 2)
-            {
-                Console.Write("Data folder path (leave empty to read from Steam Web API): ");
-                dataPath = Console.ReadLine();
-            }
-            else
-                dataPath = args[1];
-            load = !string.IsNullOrEmpty(dataPath);
-
-            // played minutes threshold
-            if (args == null || args.Length < 3)
-            {
-                Console.Write("Played game threshold (minutes, default 0): ");
-                string t = Console.ReadLine();
-                int.TryParse(t, out threshold);
-            }
-            else
-                int.TryParse(args[2], out threshold);
-            #endregion
-
-            StreamWriter logFile = new StreamWriter("log.txt");
-            logFile.AutoFlush = true;
             int progress = 0;
-            Console.WriteLine("Reading stats...");
-
             try
             {
-                string gamesPath = (string.IsNullOrEmpty(dataPath) ? "" : dataPath + "\\") + "games.txt";
-                string gamesReq = string.Format(GetOwnedGamesFormat, steamId);
-                string jsonGames = load ? ReadFile(gamesPath) : Execute(gamesReq);
-                if (string.IsNullOrEmpty(jsonGames))
-                {
-                    if (load)
-                    {
-                        load = false;
-                        jsonGames = Execute(gamesReq);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Could not read games from Steam API");
-                        return;
-                    }
-                }
-                if (!load)
-                    using (StreamWriter sw = new StreamWriter(gamesPath))
-                    {
-                        sw.Write(jsonGames);
-                        sw.Close();
-                    }
+                string gamesReq = string.Format(GetOwnedGamesFormat, SteamId);
+                string jsonGames = Execute(gamesReq);
 
                 JObject objGames = (JObject)JObject.Parse(jsonGames)["response"];
                 JArray games = (JArray)objGames["games"];
@@ -116,7 +38,7 @@ namespace Steam
                 int playedMinutes = 0;
                 int withAchiev = 0;
                 int achievCount = 0;
-                int achievPercent = 0;
+                double achievPercent = 0;
                 int achievTotal = 0;
                 int achievNotPlayed = 0;
                 int achievNone = 0;
@@ -125,26 +47,19 @@ namespace Steam
                 #region calculate
                 foreach (JObject g in games)
                 {
-                    Console.Write("\r{0}%  ", Percent(progress++, count));
+                    Console.Write("\r{0}%  ", progress++ * 100 / count);
                     string id = g["appid"].ToString();
                     int minutes = int.Parse(g["playtime_forever"].ToString());
-                    bool gameNotPlayed = minutes <= threshold;
+                    bool gameNotPlayed = minutes == 0;
 
                     if (gameNotPlayed)
                         notPlayed++;
                     else
                         playedMinutes += minutes;
 
-                    string gPath = string.Format("{0}{1}.txt", string.IsNullOrEmpty(dataPath) ? "" : dataPath + "\\", id);
-                    string jsonAchiev = load ? ReadFile(gPath) : Execute(string.Format(GetPlayerAchievementsFormat, id, steamId));
+                    string jsonAchiev = Execute(string.Format(GetPlayerAchievementsFormat, id, SteamId));
                     if (string.IsNullOrEmpty(jsonAchiev))
                         continue;
-                    if (!load)
-                        using (StreamWriter sw = new StreamWriter(gPath))
-                        {
-                            sw.Write(jsonAchiev);
-                            sw.Close();
-                        }
 
                     JObject objAchiev = (JObject)JObject.Parse(jsonAchiev)["playerstats"];
                     if (objAchiev == null || !bool.Parse(objAchiev["success"].ToString()))
@@ -168,7 +83,6 @@ namespace Steam
                         if (int.Parse(a["achieved"].ToString()) > 0)
                             ac++;
                     }
-                    logFile.WriteLine("{0}: {1}/{2} in {3}m", objAchiev["gameName"], ac, at, minutes);
 
                     if (ac == 0)
                         achievNone++;
@@ -180,9 +94,8 @@ namespace Steam
                     }
                     else
                     {
-                        int ap = Percent(ac, at);
                         achievCount += ac;
-                        achievPercent += ap;
+                        achievPercent += ac * 100.0 / at;
                     }
                 }
                 #endregion
@@ -190,11 +103,9 @@ namespace Steam
 
                 int gamesWithAchievements = withAchiev - achievNotPlayed - achievNone;
                 string result = string.Format(
-                    "Hours played = {0}\r\nGame count = {1}\r\nGames not played = {2}\r\nGames with achievements = {3}, Not played = {4}, No achievements = {5}\r\nPerfect games = {6}\r\nTotal achievement percent = {7} for {8} games\r\nAverage completion rate = {9}\r\nAchievements = {10} / {11} ({12}%)\r\n",
-                    (playedMinutes + 30) / 60, count, notPlayed, withAchiev, achievNotPlayed, achievNone, perfectGames, achievPercent, gamesWithAchievements, Percent(achievPercent, gamesWithAchievements * 100), achievCount, achievTotal, Percent(achievCount, achievTotal));
+                    "Hours played = {0}\r\nGame count = {1}\r\nGames not played = {2}\r\nGames with achievements = {3}, Not played = {4}, No achievements = {5}\r\nPerfect games = {6}\r\nTotal achievement percent = {7} for {8} games\r\nAverage completion rate = {9}\r\nAchievements = {10} / {11}\r\n",
+                    (playedMinutes + 30) / 60, count, notPlayed, withAchiev, achievNotPlayed, achievNone, perfectGames, achievPercent, gamesWithAchievements, achievPercent / gamesWithAchievements, achievCount, achievTotal);
                 Console.WriteLine(result);
-                logFile.WriteLine(result);
-                Console.WriteLine("Stats saved to file log.txt");
             }
             catch (Exception ex)
             {
@@ -203,7 +114,6 @@ namespace Steam
             finally
             {
                 Console.ReadKey(true);
-                logFile.Close();
             }
         }
     }
