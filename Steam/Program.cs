adf4f9862c08411f00ca2945d567aa029e4e4c1e
cpvoinea <cpvoinea@gmail.com>
2017-05-09 12:38:18 +0300

    load from file

--- Steam/Program.cs
+++ Steam/Program.cs
@@ -8,52 +8,95 @@ namespace Steam
 {
     partial class Program
     {
+        const string fileName = "games.csv";
+
+        static List<Game> ReadFromApi(string steamId)
+        {
+            int progress = 0;
+            Console.WriteLine("Reading stats...");
+
+            OwnedGamesResponse response = ApiRequest.GetOwnedGames(steamId).response;
+            int count = response.game_count;
+            var games = response.games.Select(g => new Game(g.appid, g.name, g.playtime_forever, g.img_icon_url, g.img_logo_url)).ToList();
+            games.ForEach(g =>
+            {
+                Console.Write("\r{0}%  ", progress++ * 100 / count);
+                var stats = ApiRequest.GetPlayerAchievements(steamId, g.Id).playerstats;
+                if (stats.success && stats.achievements != null && stats.achievements.Length > 0)
+                    g.SetAchiev(stats.achievements.Length, stats.achievements.Count(a => a.achieved > 0));
+            });
+            Console.WriteLine("\rDone.\r\n");
+
+            return games;
+        }
+
+        static List<Game> Load()
+        {
+            List<Game> games = new List<Game>();
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string[] vals = sr.ReadLine().Split(',');
+                    int l = vals.Length;
+
+                    string name = vals[1];
+                    if (l > 7)
+                        for (int i = 2; i < l - 5; i++)
+                            name += ", " + vals[i];
+                    name = name.Trim('"');
+
+                    var g = new Game(int.Parse(vals[0]), name, int.Parse(vals[l - 5]), vals[l - 2], vals[l - 1]);
+                    g.SetAchiev(int.Parse(vals[l - 4]), int.Parse(vals[l - 3]));
+
+                    games.Add(g);
+                }
+            }
+
+            return games;
+        }
+
         static void Save(List<Game> games)
         {
-            using (StreamWriter sw = new StreamWriter("games.csv"))
+            using (StreamWriter sw = new StreamWriter(fileName))
             {
                 sw.WriteLine(Game.CsvHeader);
                 games.ForEach(g => sw.WriteLine(g.ToString()));
             }
-            Console.WriteLine("Saved to games.csv");
+            Console.WriteLine("Saved to {0}", fileName);
         }
 
         static void PrintPerfectGames(List<Game> games)
         {
-            Console.WriteLine("{0} perfect games: ", games.Count);
-            games.Where(g => g.Playtime > 0 && g.AchievCount > 0 && g.AchievDone == g.AchievCount).ToList().ForEach(g => Console.WriteLine(g.Name));
+            var perfect = games.Where(g => g.Playtime > 0 && g.AchievCount > 0 && g.AchievDone == g.AchievCount).ToList();
+            Console.WriteLine("{0} perfect games: ", perfect.Count);
+            perfect.ForEach(g => Console.WriteLine(g.Name));
         }
 
         static void Main(string[] args)
         {
             try
             {
-                Console.Write("SteamId: ");
-                string id = Console.ReadLine();
-                if (string.IsNullOrEmpty(id))
-                    id = UserData.SteamId;
-
                 int option = 1;
                 //Console.Write("Option (1=Print perfect games): ");
                 //int.TryParse(Console.ReadKey().KeyChar.ToString(), out option);
                 //Console.WriteLine();
 
-                int progress = 0;
-                Console.WriteLine("Reading stats...");
-
-                OwnedGamesResponse response = ApiRequest.GetOwnedGames(id).response;
-                int count = response.game_count;
-                var games = response.games.Select(g => new Game(g.appid, g.name, g.playtime_forever, g.img_icon_url, g.img_logo_url)).ToList();
-                games.ForEach(g =>
+                List<Game> games;
+                if (File.Exists(fileName))
+                    games = Load();
+                else
                 {
-                    Console.Write("\r{0}%  ", progress++ * 100 / count);
-                    var stats = ApiRequest.GetPlayerAchievements(id, g.Id).playerstats;
-                    if (stats.success)
-                        g.SetAchiev(stats.achievements.Length, stats.achievements.Count(a => a.achieved > 0));
-                });
-                Console.WriteLine("\rDone.\r\n");
+                    Console.Write("SteamId: ");
+                    string id = Console.ReadLine();
+                    if (string.IsNullOrEmpty(id))
+                        id = UserData.SteamId;
 
-                Save(games);
+                    games = ReadFromApi(id);
+                    Save(games);
+                }
+
                 if (option == 1)
                     PrintPerfectGames(games);
             }
